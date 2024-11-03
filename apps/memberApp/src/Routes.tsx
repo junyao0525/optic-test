f5b57d6f7eadbaab8c3c0ce8c600a8f3e041763e
junyao <junyao0375@gmail.com>
2024-11-03 17:04:59 +0800

    add routes

---
+++ apps/memberApp/src/Routes.tsx
@@ -0,0 +1,25 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+// const Providers = () => {
+//   return <></>;
+// };
+
+const Routes = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default Routes;
