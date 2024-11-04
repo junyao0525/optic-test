d38b6c94c426626114b672350c7e27a3441bf189
junyao <junyao0375@gmail.com>
2024-11-04 17:50:46 +0800

    feat (member) : add theme

--- apps/memberApp/src/screens/Home.tsx
+++ apps/memberApp/src/screens/Home.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {Colors} from '../themes';
 
 const HomeScreen = () => {
+  const handleOnPress = useCallback(() => {
+    console.log('button pressed');
+  }, []);
+
   return (
-    <View>
+    <View style={[styles.container]}>
       <Text>HomeScreen</Text>
+      <Button onPress={handleOnPress} title="buwwwtton" color={Colors.Orange} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.BackgroundColors,
+  },
+});
 
 export default HomeScreen;
---
+++ apps/memberApp/src/themes.ts
@@ -0,0 +1,5 @@
+export const Colors = {
+  BackgroundColors: 'Transparent',
+  primary: '#0099ff',
+  Orange: '#FF4000',
+};
