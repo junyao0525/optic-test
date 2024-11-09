67168166b0eaf3b3140c9f6a2fdafbe5b9df7cce
junyao <junyao0375@gmail.com>
2024-11-09 17:45:11 +0800

    feat (memberApp) : setup text and color

--- apps/memberApp/src/themes.ts
+++ apps/memberApp/src/themes.ts
@@ -1,5 +1,83 @@
+import {StyleSheet} from 'react-native';
+
+export const FontFamilies = {
+  black: 'Nunito-Black',
+  blackItalic: 'Nunito-BlackItalic',
+  bold: 'Nunito-Bold',
+  boldItalic: 'Nunito-BoldItalic',
+  extraBold: 'Nunito-ExtraBold',
+  extraBoldItalic: 'Nunito-ExtraBoldItalic',
+  extraLight: 'Nunito-ExtraLight',
+  extraLightItalic: 'Nunito-ExtraLightItalic',
+  italic: 'Nunito-Italic',
+  light: 'Nunito-Light',
+  lightItalic: 'Nunito-LightItalic',
+  medium: 'Nunito-Medium',
+  mediumItalic: 'Nunito-MediumItalic',
+  regular: 'Nunito-Regular',
+  semiBold: 'Nunito-SemiBold',
+  semiBoldItalic: 'Nunito-SemiBoldItalic',
+} as const;
+
 export const Colors = {
   BackgroundColors: 'Transparent',
   primary: '#0099ff',
   Orange: '#FF4000',
+  'light-grey': '#2F4F4F',
+  'light-green': '#8FBC8F',
+  'light-cyan': '#E0FFFF',
+  'light-blue': '#B0C4DE',
+  black: '#000000',
+  white: '#FFFFFF',
 };
+
+export const TextStyle = StyleSheet.create({
+  H1: {
+    fontSize: 32,
+    fontFamily: FontFamilies.black,
+  },
+  H2: {
+    fontSize: 28,
+    fontFamily: FontFamilies.black,
+  },
+  H3: {
+    fontSize: 24,
+    fontFamily: FontFamilies.black,
+  },
+  H1B: {
+    fontSize: 32,
+    fontFamily: FontFamilies.bold,
+  },
+  H2B: {
+    fontSize: 28,
+    fontFamily: FontFamilies.bold,
+  },
+  H3B: {
+    fontSize: 24,
+    fontFamily: FontFamilies.bold,
+  },
+  P1: {
+    fontSize: 18,
+    fontFamily: FontFamilies.regular,
+  },
+  P2: {
+    fontSize: 16,
+    fontFamily: FontFamilies.regular,
+  },
+  P3: {
+    fontSize: 14,
+    fontFamily: FontFamilies.regular,
+  },
+  P1B: {
+    fontSize: 18,
+    fontFamily: FontFamilies.semiBold,
+  },
+  P2B: {
+    fontSize: 16,
+    fontFamily: FontFamilies.semiBold,
+  },
+  P3B: {
+    fontSize: 14,
+    fontFamily: FontFamilies.semiBold,
+  },
+});
