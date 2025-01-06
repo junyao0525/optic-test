28134291c594bbfc4ca3e79318394e2bbf31f9a0
yuzhe <tuna23423@gmail.com>
2025-01-06 21:18:57 +0800

    change eslint config

--- apps/memberApp/.eslintrc.js
+++ apps/memberApp/.eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native',
   rules: {
-    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'prettier/prettier': [{endOfLine: 'auto'}],
   },
 };
