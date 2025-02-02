b084927460eac2b848a47c11f2b11f05d2265b92
yuzhe <tuna23423@gmail.com>
2025-02-02 14:16:31 +0800

    feat: color vision test page

--- apps/memberApp/src/components/Button.tsx
+++ apps/memberApp/src/components/Button.tsx
@@ -6,12 +6,21 @@ type Props = {
   variant?: string;
   onPress: () => void;
   title: string;
+  containerStyle?: any;
+  textStyle?: any;
 };
 
 const Button = (props: Props) => {
   return (
-    <Pressable style={styles.fbContainer} onPress={props.onPress}>
-      <Text style={[TextStyle.P2, styles.title]}>{props.title}</Text>
+    <Pressable
+      style={props.containerStyle ? props.containerStyle : styles.fbContainer}
+      onPress={props.onPress}>
+      <Text
+        style={
+          props.textStyle ? props.textStyle : [TextStyle.P2, styles.title]
+        }>
+        {props.title}
+      </Text>
     </Pressable>
   );
 };
--- apps/memberApp/src/screens/ColorVision/ColorVision.tsx
+++ apps/memberApp/src/screens/ColorVision/ColorVision.tsx
@@ -1,17 +1,44 @@
-import React, {ReactNode, useEffect, useState} from 'react';
-import {StyleSheet, Text, useWindowDimensions, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View, Image, useWindowDimensions} from 'react-native';
 import {Colors, TextStyle} from '../../themes';
 import Button from '../../components/Button';
 import {useNavigation} from '@react-navigation/native';
 
+const ColorVisionImage = require('../../../assets/images/home/color-vision.png');
 const ColorVision = () => {
   const {navigate} = useNavigation();
   const {width, height} = useWindowDimensions();
-
   return (
     <View style={[styles.container, {width, minHeight: height}]}>
-      <Text style={[TextStyle.H3, styles.text]}>Color Vision</Text>
-      <Button title="test" onPress={() => {}} />
+      <Image source={ColorVisionImage} style={styles.image} />
+      <Text style={[TextStyle.H3, styles.text]}>Color Vision Test</Text>
+
+      <View style={styles.gridContainer}>
+        <Button
+          title="1"
+          onPress={() => {}}
+          containerStyle={styles.buttonContainer}
+          textStyle={TextStyle.H1B}
+        />
+        <Button
+          title="2"
+          onPress={() => {}}
+          containerStyle={styles.buttonContainer}
+          textStyle={TextStyle.H1B}
+        />
+        <Button
+          title="3"
+          onPress={() => {}}
+          containerStyle={styles.buttonContainer}
+          textStyle={TextStyle.H1B}
+        />
+        <Button
+          title="4"
+          onPress={() => {}}
+          containerStyle={styles.buttonContainer}
+          textStyle={TextStyle.H1B}
+        />
+      </View>
     </View>
   );
 };
@@ -23,11 +50,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: Colors.backgroundColor,
   },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
   text: {
     textAlign: 'center',
     marginBottom: 20,
     fontWeight: '400',
     color: Colors.black,
+  },
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 40,
+  },
+  buttonContainer: {
+    width: '30%',
+    backgroundColor: Colors.lightBlue,
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
