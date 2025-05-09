ea02fb0528bf448a54da894e402c17e00dd178e1
junyao <junyao0375@gmail.com>
2025-05-10 00:40:52 +0800

    feat : env setup

--- apps/memberApp/src/api/python.ts
+++ apps/memberApp/src/api/python.ts
@@ -1,7 +1,8 @@
-import {useMutation} from '@tanstack/react-query';
-import {DetectFaceApi} from '@vt/core/apis/app/python';
+import { BACKEND_API_URL } from '@env';
+import { useMutation } from '@tanstack/react-query';
+import { DetectFaceApi } from '@vt/core/apis/app/python';
 
-const API_BASE_URL = 'http://192.168.100.8:8000';
+const API_BASE_URL = BACKEND_API_URL;
 // api/python.ts
 
 export const useDetectFaceAPI = () => {
@@ -11,14 +12,18 @@ export const useDetectFaceAPI = () => {
     FormData
   >({
     mutationFn: async (formData: FormData) => {
+      console.log(
+        'Sending request to:',
+        API_BASE_URL + '/mediapipe/detect-face/',
+      );
       const response = await fetch(API_BASE_URL + '/mediapipe/detect-face/', {
         method: 'POST',
         body: formData,
       });
 
       if (!response.ok) {
-        // You can handle specific errors here if needed
-        throw new Error('Face detection failed');
+        console.log('Error response:', response);
+        throw new Error('Face detection failed : ' + response.statusText);
       }
 
       return (await response.json()) as DetectFaceApi['Response'];
