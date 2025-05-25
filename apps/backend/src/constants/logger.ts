24adf706cf8022bdccbccfbff84f21e2ad044811
junyao <junyao0375@gmail.com>
2025-05-25 18:29:09 +0800

    delete file

--- apps/backend/src/constants/logger.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import { APP_NAME, isDev } from "./config";
-
-export const LOGGER_PATH = isDev
-  ? path.join(__dirname, "../..", "logs", APP_NAME)
-  : "/var/log/" + APP_NAME;
